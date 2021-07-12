55de85a8565e84ea9936405a2dd1ed7b75e91e41
yedidyaroy <76555587+yedidyaroy@users.noreply.github.com>
2021-07-12 18:16:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var database;
+var gameState=0;
+var playerCount;
+var form,player,game;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+  game= new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+
+}
